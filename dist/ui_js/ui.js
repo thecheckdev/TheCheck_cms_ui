632368c932844b3a8174a4bd97fd265ddbc3f573
ynlee <ynlee@thecheck.co.kr>
2021-07-28 18:03:31 +0900

    first commit

---
+++ dist/ui_js/ui.js
@@ -0,0 +1,1 @@
+document.createElement("article"),document.createElement("section"),document.createElement("aside"),document.createElement("nav"),document.createElement("header"),document.createElement("footer"),document.createElement("main"),getInternetExplorerVersion();
